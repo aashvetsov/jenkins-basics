b24c112213f4da9982297763279dd843024533ab
Artem Shvetsov <artem.shvetsov@pm.bet>
2021-02-26 15:16:13 +0200

    Initial commit.

---
+++ Jenkins-test-project/Jenkins-test-projectTests/Jenkins_test_projectTests.swift
@@ -0,0 +1,33 @@
+//
+//  Jenkins_test_projectTests.swift
+//  Jenkins-test-projectTests
+//
+//  Created by Artem Shvetsov on 26.02.2021.
+//
+
+import XCTest
+@testable import Jenkins_test_project
+
+class Jenkins_test_projectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
